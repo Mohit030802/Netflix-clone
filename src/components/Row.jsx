07985cdbf029442bdc99237965608bae265cca0b
Mohit030802 <mohitom2002@gmail.com>
2022-05-31 14:14:36 +0530

    first commit

---
+++ src/components/Row.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Row = ({title,fetchURL}) => {
+  return (
+    <>
+      <h2 className='text-white font-bold md:text-4xl p-4 '>{title}</h2>
+      <div className='relative flex items-center'>
+
+      </div>
+    </>
+  )
+}
+
+export default Row
