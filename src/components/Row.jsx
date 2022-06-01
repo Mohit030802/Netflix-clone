6c8effa96d2f11abd9b9074a486f5fccc4f4e2e7
Mohit030802 <mohitom2002@gmail.com>
2022-06-01 13:55:28 +0530

    page

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import Movie from './Movie'
+import {MdChevronLeft,MdChevronRight} from 'react-icons/md'
 
-const Row = ({title,fetchURL}) => {
+const Row = ({ title, fetchURL }) => {
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    axios.get(fetchURL).then((response) => {
+      setMovies(response.data.results)
+    })
+  }, [fetchURL])
+  console.log(movies)
   return (
     <>
       <h2 className='text-white font-bold md:text-4xl p-4 '>{title}</h2>
-      <div className='relative flex items-center'>
+      <div className='relative flex items-center group'>
+        <MdChevronLeft className='bg-white rounded-full cursor-pointer z-10 hidden opacity-50 hover:opacity-100 group-hover:block' size={40}/>
+        <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative '>
+          {movies.map((item, id) => (
+            <Movie key={id} item={item}/>
+          ))}
 
+        </div>
+        <MdChevronRight className='bg-white rounded-full cursor-pointer z-10 hidden opacity-50 hover:opacity-100 group-hover:block' size={40}/>
       </div>
     </>
   )
