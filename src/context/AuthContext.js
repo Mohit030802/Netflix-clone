3a6bcb36056ca79987e9d1c38b55b442536b2115
Mohit030802 <mohitom2002@gmail.com>
2022-06-03 14:19:13 +0530

    firebasee

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,9 +1,11 @@
 import { createContext,useContext,useEffect,useState } from "react";
-import {aut} from '../firebase'
+import {auth} from '../firebase'
 import { createUserWithEmailAndPassword,signInWithEmailAndPassword,signOut,onAuthStateChanged } from "firebase/auth";
 const AuthContext=createContext();
 export function AuthContextProvide({children}){
+    const [user,setUser]=useState({})
     return(
+        
         <AuthContext.Provide>
         {children}
         </AuthContext.Provide>
